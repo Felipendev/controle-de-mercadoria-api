9533f264c4f5f564ae50881af4acf8bcd83538e0
felip <felipe.psic@outlook.com.br>
2021-09-14 08:54:40 -0300

    alteração no pom.xlm, add de @data na classe clienteDTO e alteração no arquivo aplication.properties

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO cliente(codigo,	nome, sobrenome, data_recebimento, status_produto) VALUES('0001','Cliente1','Teste1','2021-09-13','0')
+INSERT INTO cliente(codigo,	nome, sobrenome, data_recebimento, status_produto) VALUES('0002','Cliente2','Teste2','2021-09-13','0')
+INSERT INTO cliente(codigo,	nome, sobrenome, data_recebimento, status_produto) VALUES('0003','Cliente3','Teste3','2021-09-13','0')
+INSERT INTO cliente(codigo,	nome, sobrenome, data_recebimento, status_produto) VALUES('0004','Cliente4','Teste4','2021-09-13','0')
+

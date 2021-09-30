6b7760a52d08af37ce8f4283374170628e631629
felip <felipe.psic@outlook.com.br>
2021-09-30 09:42:31 -0300

    add de logs

--- src/main/java/com/controle/mercadoria/controller/ClienteController.java
+++ src/main/java/com/controle/mercadoria/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.controle.mercadoria.dto.response.MessageResponseDTO;
 import com.controle.mercadoria.exception.ClienteNotFoundException;
 import com.controle.mercadoria.service.ClienteService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,6 +23,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@Log4j2
 @RequestMapping("/api/v1/cliente")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ClienteController {
@@ -31,31 +33,45 @@ public class ClienteController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO criaCliente(@RequestBody @Valid ClienteDTO clienteDTO) {
-        return clienteService.criaCliente(clienteDTO);
+        log.info("[Inicia] ClienteController - criaCliente");
+        MessageResponseDTO messageResponseDTO = clienteService.criaCliente(clienteDTO);
+        log.info("[Finaliza] ClienteController - criaCliente");
+        return messageResponseDTO;
     }
 
     @CrossOrigin
     @GetMapping(value = "/listAll")
     public List<ClienteDTO> buscaTodos() {
-       return clienteService.buscaPorTodos();
+        log.info("[Inicia] ClienteController - buscaTodos");
+        List<ClienteDTO> clienteDTOS = clienteService.buscaPorTodos();
+        log.info("[Finaliza] ClienteController - buscaTodos");
+        return clienteDTOS;
     }
 
     @CrossOrigin
     @GetMapping(value = "/{id}")
     public ClienteDTO buscaPorId(@PathVariable Long id) throws ClienteNotFoundException {
-        return clienteService.buscaPorId(id);
+        log.info("[Inicia] ClienteController - buscaPorId");
+        ClienteDTO clienteDTO = clienteService.buscaPorId(id);
+        log.info("[Finaliza] ClienteController - buscaPorId");
+        return clienteDTO;
     }
 
     @CrossOrigin
     @PutMapping(value = "/{id}")
     public MessageResponseDTO atualizaPorId(@PathVariable Long id, @RequestBody @Valid ClienteDTO clienteDTO) throws ClienteNotFoundException {
-        return clienteService.atualizaPorId(id, clienteDTO);
+        log.info("[Inicia] ClienteController - atualizaPorId");
+        MessageResponseDTO messageResponseDTO = clienteService.atualizaPorId(id, clienteDTO);
+        log.info("[Finaliza] ClienteController - atualizaPorId");
+        return messageResponseDTO;
     }
 
     @CrossOrigin
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletaCliente(@PathVariable Long id) throws ClienteNotFoundException {
+        log.info("[Inicia] ClienteController - deletaCliente");
         clienteService.delete(id);
+        log.info("[Finaliza] ClienteController - deletaCliente");
     }
 }

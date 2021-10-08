ade492996d98469757917bbfe6273d413fb676c5
felip <felipe.psic@outlook.com.br>
2021-10-08 11:49:05 -0300

    Remove packt de autenticação

--- src/main/java/com/controle/mercadoria/ControleMercadoriaJavaApplication.java
+++ src/main/java/com/controle/mercadoria/ControleMercadoriaJavaApplication.java
@@ -2,21 +2,12 @@ package com.controle.mercadoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ControleMercadoriaJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleMercadoriaJavaApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
 	}
 
 }

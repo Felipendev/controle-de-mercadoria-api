2d474790a3a7e2670e0187f7ffe393485c78a164
felip <felipe.psic@outlook.com.br>
2021-10-10 11:26:41 -0300

    Remove classes admin

--- src/main/java/com/controle/mercadoria/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.controle.mercadoria.repository;
-//
-//import com.controle.mercadoria.model.Admin;
-//import org.springframework.data.repository.PagingAndSortingRepository;
-//
-//public interface AdminRepository extends PagingAndSortingRepository<Admin, Long> {
-//    Admin findByUserName(String userName);
-//}
